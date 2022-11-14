5ee562c3e57d5ac4eb6255293771403b9bf3f5ac
Carlos Trejo <37058445+CarlosGTrejo@users.noreply.github.com>
2022-11-14 00:37:32 -0600

    vite config saves to docs instead of dist now

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/Novena-54/",
+  build: {
+    outDir: './docs'
+  },
   plugins: [svelte()]
 })
