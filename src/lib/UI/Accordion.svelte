c1db4d4ce528861a1b17607f0a84f0e8bb278eb9
Carlos Trejo <37058445+CarlosGTrejo@users.noreply.github.com>
2022-11-13 22:36:37 -0600

    Rebuilt using yarn, vite, and made a new theme 🚀

---
+++ src/lib/UI/Accordion.svelte
@@ -0,0 +1,48 @@
+<script>
+  export let open = false;
+  import { slide } from "svelte/transition";
+  function onClick() {
+    open = !open;
+  }
+</script>
+
+<div class="accordion">
+  <button class="header" on:click={onClick}>
+    <slot name="header" />
+  </button>
+  {#if open}
+  <div class="content" transition:slide>
+    <slot name="content" />
+  </div>
+  {/if}
+</div>
+
+<style>
+  button {
+    background: none;
+    color: inherit;
+    border: none;
+    padding: 0;
+    margin: 0;
+    text-align: justify;
+    font: inherit;
+    outline: none;
+  }
+  .header {
+      font-weight: bold;
+      user-select: none;
+      text-align: center;
+  }
+  .accordion {
+      border-top: 1px solid;
+      border-color: var(--text);
+      padding: 0.5em;
+      margin: 0 2em;
+  }
+  .content {
+      max-width: 40ch;
+      text-align: justify;
+      margin: 0.5em auto 0;
+  }
+
+</style>
